621737fcd8cd61edd02a518d1c7c765d1937753a
neiji-hadhri <neiji.hadhri@laplateforme.io>
2023-01-02 15:20:31 +0100

    jour 01

---
+++ Jour01/Aller plus loin.py
@@ -0,0 +1,10 @@
+
+while true :
+    phrase =input ("écrit ta phrase pour savoir si elle contient la lettre 'e': ")
+    print("e" in phrase) 
+    if "e" in phrase or "E" in phrase:
+        print("la lettre 'e' est bien presente dans la  phrase")
+    else:
+        print("la lettre 'e' n'est pas presente dans la phrase ")
+
+
---
+++ Jour01/Job 8.py
@@ -0,0 +1,4 @@
+num1= 3
+num2= 14
+somme=(num1+num2)
+print(somme)
